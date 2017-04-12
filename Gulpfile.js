db40b0d51fc8850fe9f7fb3c177f44825a1587bb
Thibault Vlacich <thibault.vlacich@gmail.com>
2017-04-13 00:36:27 +0200

    Gulp task to optimize images

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,18 +1,29 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var cleanCSS = require('gulp-clean-css');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
 
-gulp.task('build', function() {
+// Task for compiling SCSS files
+gulp.task('compileSCSS', function () {
   gulp.src('./src/main/webapp/resources/scss/app.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(cleanCSS())
+      .pipe(sass().on('error', sass.logError)) // Log compilation errors
+      .pipe(cleanCSS()) // Minify output CSS
       .pipe(gulp.dest('./src/main/webapp/resources/core/css/'));
 });
 
-// Watch task
-gulp.task('watch', function() {
-  gulp.watch('./src/main/webapp/resources/scss/**/*.scss', ['build']);
+// Task for optimizing images
+gulp.task('optimizeImages', function () {
+  gulp.src('./src/main/webapp/resources/*/images/*', { base: './' })
+      .pipe(imagemin())
+      .pipe(gulp.dest('./'));
 });
 
-// Set default task
-gulp.task('default', ['watch']);
+// Watch task
+gulp.task('watch', ['optimizeImages'], function() {
+  // Automatically recompile SCSS files when modified
+  gulp.watch('./src/main/webapp/resources/scss/**/*.scss', ['compileSCSS']);
+});
+
+// Set default tasks
+gulp.task('build', ['compileSCSS', 'optimizeImages']);
+gulp.task('default', ['compileSCSS', 'optimizeImages']);
