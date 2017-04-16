eb7bbffa55ee8550ffa93c7348a34f790bc54df5
Thibault Vlacich <thibault.vlacich@gmail.com>
2017-04-16 23:03:41 +0200

    [Gulpfile] Use ES6 syntax

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,18 +1,18 @@
-const elixir = require('laravel-elixir');
+const elixir = require('laravel-elixir')
 
-require('laravel-elixir-imagemin');
+require('laravel-elixir-imagemin')
 
 const paths = {
   'SOURCE': 'src/main/webapp/resources',
   'DESTINATION': 'src/main/webapp/public'
-};
+}
 
 // Config Elixir
-elixir.config.assetsPath = paths.SOURCE;
-elixir.config.publicPath = paths.DESTINATION;
-elixir.config.notifications = false;
+elixir.config.assetsPath = paths.SOURCE
+elixir.config.publicPath = paths.DESTINATION
+elixir.config.notifications = false
 
-elixir(function (mix) {
+elixir(mix => {
   // Compile main SASS file
   mix.sass('app.scss')
      // Compile main JS file
@@ -20,5 +20,5 @@ elixir(function (mix) {
      // Optimize then copy image files to the public dir
      .imagemin(paths.SOURCE+'/images', paths.DESTINATION+'/images')
      // Copy Bootstrap fonts to the public dir
-     .copy(paths.SOURCE+'/bootstrap/fonts', paths.DESTINATION+'/fonts');
-});
+     .copy(paths.SOURCE+'/bootstrap/fonts', paths.DESTINATION+'/fonts')
+})
